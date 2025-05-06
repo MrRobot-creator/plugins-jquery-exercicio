4a6a55fea7d05a41447b7de392ab9f4a15786e5d
Matheus Nobre <matheusnsenado6@gmail.com>
2025-05-06 18:04:23 -0300

    finalizado

--- js/main.js
+++ js/main.js
@@ -1,42 +1,69 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
-})
+$(document).ready(function() {
+    $('#carousel-imagens').slick({
+        autoplay: true
+});
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
+$('#telefone').mask('(00) 00000-0000')
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
+$('#cpf').mask('000.000.000-00')
+
+$('#cep').mask('00000-000')
 
 $('form').validate({
     rules: {
         nome: {
-            requerido: true
+            required: true
         },
         email: {
-            requerido: true,
+            required: true,
             email: true
         },
         telefone: {
-            requerido: true
+            required: true
         },
         endereco: {
-            requerido: true
+            required: true
         },
         cep: {
-            requerido: true
+            required: true
         },
         cpf: {
-            requerido: true
+            required: true
+            }
         },
+        messages: {
+            nome: {
+                required: "Por favor, insira seu nome completo",
+                minlength: "O nome deve ter pelo menos 3 caracteres"
+            },
+            email: {
+                required: "Por favor, insira seu e-mail",
+                email: "Por favor, insira um e-mail válido"
+            },
+            telefone: {
+                required: "Por favor, insira seu telefone",
+                minlength: "Telefone incompleto"
+            },
+            cpf: {
+                required: "Por favor, insira seu CPF",
+                minlength: "CPF incompleto"
+            },
+            endereco: {
+                required: "Por favor, insira seu endereço",
+                minlength: "Endereço muito curto"
+            },
+            cep: {
+                required: "Por favor, insira seu CEP",
+                minlength: "CEP incompleto"
+            }
     },
-    submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
+    submitHandler: function(form) {
+        alert('Formulário enviado com sucesso!');
     },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
+    invalidHandler: function(evento, validador) {
+        let camposIncorretos = validador.numberOfInvalids();
+        console.log(camposIncorretos)
+        alert(`Por favor, corrija os ${camposIncorretos} campo(s) destacado(s)`);
     }
+    })
 })
